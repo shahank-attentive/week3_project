657472e19cb39b3fb195d134045edb7e76571442
Shashank <shankp108@gmail.com>
2023-03-03 12:31:42 +0530

    implemented m2m

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Employee, Device, History
+from .models import Employee, Device, Status, History
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,26 +9,55 @@ class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class DeviceSerializer(serializers.HyperlinkedModelSerializer):
-    current_user = EmployeeSerializer(many=False)
+    current_user = EmployeeSerializer(many=True)
 
     class Meta:
         model = Device
         fields = ["id", "name", "model", "current_user"]
 
-    # def create(self, validated_data):
-    #     data = validated_data.get("current_user")
-    #     print(data)
-    #     serializer = DeviceSerializer(data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-
-    #     return Device.objects.create(**validated_data)
+    def create(self, validated_data):
+        data = validated_data.pop("current_user")
+        print("data", type(data))  # data is list of dict
+        print("to print", data)
+        device = Device.objects.create(**validated_data)
+        for a in data:  # fetching the list
+            Employee.objects.create(device=device, **a)
+        return device
 
 
 class HistorySerializer(serializers.HyperlinkedModelSerializer):
+    # emp= EmployeeSerializer(many=True)
+    # dev=DeviceSerializer()
     class Meta:
         model = History
         fields = ["id", "emp", "dev", "time"]
+
+
+class StatusSerializer(serializers.HyperlinkedModelSerializer):
+    # history2 = HistorySerializer2()
+
+    class Meta:
+        model = Status
+        fields = ["id", "emp", "dev", "time"]
+
+    def create(self, validated_data):
+        # a = Employee.objects.get(id=1)
+        # if validated_data["emp"] == None:
+        # validated_data["emp"]=a
+        print("vaildated _data", validated_data)  # dict type
+        data = validated_data
+        print("to print", data)  # queryset
+        his = Status.objects.create(**validated_data)
+        History.objects.create(**data)
+        return his
+
+    # def create(self, validated_data):
+    #     data = validated_data.pop("emp")
+    #     print("to print", data)
+    #     device = History.objects.create(**validated_data)
+    #     for a in data:
+    #         Employee.objects.create(device=device, **a)
+    #     return device
 
 
 # class EmployeeHistorySerializer(serializers.HyperlinkedModelSerializer):
